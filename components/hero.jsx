4eec68d1f145dbdd3c01952b6eb9feb9e4750e16
Oak <mickyodum@gmail.com>
2024-02-16 22:30:40 +0100

    online experience and grid

---
+++ components/hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <img src="../images/grid.png" alt="picture grid"/>
+            <h1>Online Experiences</h1>
+            <p>
+                Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </div>
+    )
+}
