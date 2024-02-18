e9c186aa03eaf2209e974eae505fdfb236a82361
Oak <mickyodum@gmail.com>
2024-02-18 20:48:09 +0100

    mapped  data function to card comp0nent

--- App.jsx
+++ App.jsx
@@ -2,20 +2,29 @@ import React from "react"
 import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import Card from "./components/card"
+import data from "./data.js"
 
 export default function App() {
+
+    const cards = data.map(card => {
+        return(
+            <Card 
+                key = {card.id}
+                img = {card.coverImg}
+                rating = {card.stats.rating}
+                reviewCount = {card.stats.reviewCount}
+                location = {card.location}
+                title = {card.title}
+                price = {card.price}
+            />
+        )
+    })
+
     return(
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                location="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            <section className="list-items">{cards}</section>
         </div>
     )
 }
