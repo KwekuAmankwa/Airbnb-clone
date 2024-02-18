32802f339440dbf1a83f936098e7b611505a07f6
Oak <mickyodum@gmail.com>
2024-02-18 18:47:04 +0100

    coaches display

--- App.jsx
+++ App.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import Navbar from "./components/navbar"
 import Hero from "./components/hero"
+import Card from "./components/card"
 
 export default function App() {
     return(
         <div>
             <Navbar />
             <Hero />
+            <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                location="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
